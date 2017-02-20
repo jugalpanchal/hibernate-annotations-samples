7cb00048b94f984c98faa7858986f91fd46f7b72
Jugal Panchal [Jugal-PC] <jugalpanchal@gmail.com>
2017-02-20 06:35:58 +0530

    Test generated db

---
+++ hibernateannotationssample/src/test/java/com/jugalpanchal/hibernate/test/createDbTest.java
@@ -0,0 +1,20 @@
+package com.jugalpanchal.hibernate.test;
+
+import static org.junit.Assert.*;
+
+import org.hibernate.Session;
+import org.junit.Test;
+
+import com.jugalpanchal.hibernate.infrastructure.Fixture;
+
+public class createDbTest {
+
+	@Test
+	public void test() throws Exception {
+		//It creates blank db.
+		Fixture fixture = new Fixture();
+		Session session = fixture.getSession();
+
+		assertNull(session);
+	}
+}
