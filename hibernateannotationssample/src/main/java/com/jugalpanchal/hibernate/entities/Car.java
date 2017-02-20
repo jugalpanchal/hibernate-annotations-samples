327f38f8044c21cfd2a3fa2198575e23a1ac2a2d
Jugal Panchal [Jugal-PC] <jugalpanchal@gmail.com>
2017-02-20 06:35:23 +0530

    Added table column attributes

--- hibernateannotationssample/src/main/java/com/jugalpanchal/hibernate/entities/Car.java
+++ hibernateannotationssample/src/main/java/com/jugalpanchal/hibernate/entities/Car.java
@@ -1,5 +1,25 @@
 package com.jugalpanchal.hibernate.entities;
 
-public class Car extends Entity {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "car")
+public class Car extends BaseEntity {
+
+	@Column(name = "name")
+	private String name;
+
+	public Car() {
+		
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 }
--- hibernateannotationssample/src/main/java/com/jugalpanchal/hibernate/entities/Company.java
+++ hibernateannotationssample/src/main/java/com/jugalpanchal/hibernate/entities/Company.java
@@ -1,5 +1,14 @@
 package com.jugalpanchal.hibernate.entities;
 
-public class Company extends Entity {
+public class Company extends BaseEntity {
+	
+	private String name;
+	
+	public String getName() {
+		return name;
+	}
 
+	public void setName(String name) {
+		this.name = name;
+	}
 }
