f913eea13bf87d41df1398b86aa68ca9f03fe379
Jugal Panchal [Jugal-PC] <jugalpanchal@gmail.com>
2017-02-21 06:34:39 +0530

    Added table and column annotation with Company class

--- hibernateannotationssample/src/main/java/com/jugalpanchal/hibernate/entities/Company.java
+++ hibernateannotationssample/src/main/java/com/jugalpanchal/hibernate/entities/Company.java
@@ -1,7 +1,14 @@
 package com.jugalpanchal.hibernate.entities;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "company")
 public class Company extends BaseEntity {
 	
+	@Column(name = "name")
 	private String name;
 	
 	public String getName() {
