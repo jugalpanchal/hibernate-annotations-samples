d45cd3315b0444ee477a93dd296574f53b47bafe
Jugal <Jugal@Jugal-PC>
2017-02-23 07:55:42 +0530

    Persist and delete user

--- hibernateannotationssample/src/test/java/com/jugalpanchal/hibernate/test/CompanyDbTransactionTester.java
+++ hibernateannotationssample/src/test/java/com/jugalpanchal/hibernate/test/CompanyDbTransactionTester.java
@@ -3,15 +3,72 @@ package com.jugalpanchal.hibernate.test;
 import static org.junit.Assert.*;
 
 import org.hibernate.Session;
+import org.hibernate.StatelessSession;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.jugalpanchal.db.entities.Car;
 import com.jugalpanchal.db.entities.Company;
 import com.jugalpanchal.db.entities.Model;
+import com.jugalpanchal.db.entities.User;
 import com.jugalpanchal.db.framework.Fixture;
 import com.jugalpanchal.db.framework.StatefullUnitOfWork;
+import com.jugalpanchal.db.framework.StatelessUnitOfWork;
+import com.jugalpanchal.db.repositories.UserRepository;
 
 public class CompanyDbTransactionTester {
+	
+	private static long userId;
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		
+		User user = new User("Jugal");
+		
+		Fixture fixture = null;
+		boolean isSaved = false;
+		try {
+			fixture = new Fixture();
+			StatelessSession session = fixture.getStatelessSession();
+			
+			StatelessUnitOfWork unitOfWork = new StatelessUnitOfWork(session);
+			session.insert(user);
+			isSaved = unitOfWork.commit();
+			
+		} catch (Exception ex) {
+			fixture.closeSessionFactory();
+			throw ex;
+		}finally {
+			userId = user.getId();//Holds to tear down object.
+			fixture.closeSession();
+		}
+		assertTrue("User is not saved.", isSaved);
+	}
+ 
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		
+		Fixture fixture = null;
+		boolean isDeleted = false;
+		try {
+			fixture = new Fixture();
+			Session session = fixture.getSession();
+			UserRepository repository = new UserRepository(session);
+			User user = repository.getUser(userId);
+			
+			StatefullUnitOfWork unitOfWork = new StatefullUnitOfWork(session);
+			session.delete(user);
+			isDeleted = unitOfWork.commit();
+			
+		} catch (Exception ex) {
+			fixture.closeSessionFactory();
+			throw ex;
+		}finally {
+			fixture.closeSession();
+		}
+		assertTrue("User is not deleted.", isDeleted);
+	}
 	
 	@Test
 	public void getCompaniesTest() throws Exception {
@@ -26,6 +83,8 @@ public class CompanyDbTransactionTester {
 	@Test
 	public void saveCompanyTest() throws Exception {
 
+		//Get user and set to every entity.
+		
 		Company company = new Company("Hyundai");
 
 		Model model1 = new Model("Sonata", company);
