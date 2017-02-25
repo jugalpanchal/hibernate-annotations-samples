3f7e4ce95cde8d216273c0f618d1d759dfb7e0a1
Jugal <Jugal@Jugal-PC>
2017-02-26 04:25:50 +0530

    insert initial data

--- hibernateannotationssample/src/main/java/initial_data.sql
+++ hibernateannotationssample/src/main/java/initial_data.sql
@@ -1,1 +1,1 @@
-INSERT INTO motordb.user (id, name) VALUES (1, "Jugal");
+INSERT INTO motordb.user (id, creation_date_time, is_active, updation_date_time, name, created_by_user_id, updated_by_user_id) VALUES (0, null, 1, null, 'jugal', 1, null);
